98d6e42f51f4bc1c0d666e805505996643ddb82f
muhammad rashad <muhammadrashad2006@gmail.com>
2022-10-08 11:14:39 +0200

    First Commit

---
+++ lib/app/views/home/home_sc.dart
@@ -0,0 +1,9 @@
+import 'package:flutter/material.dart';
+import 'package:get/get.dart';
+
+class HomeSc extends GetView {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold();
+  }
+}
